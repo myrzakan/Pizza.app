d9fd1623058ad5296de1a076f3747879c282e914
Myrzakan <alanfoureyes@gmail.com>
2023-11-15 16:10:00 +0600

    add react-router-dom and skeleton

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
+import { Route, Routes } from 'react-router-dom';
 import { Header } from './Components/Header';
-import { Main } from './Main/Main';
+import { Main } from './Pages/Main';
+import { NotFound } from './Pages/NotFound';
 import './scss/app.scss';
 export const App = () => {
   return (
     <div className="wrapper">
       <Header />
-      <Main />
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </div>
   );
 };
--- src/Pages/Main/index.jsx
+++ src/Pages/Main/index.jsx
@@ -1,21 +1,23 @@
 import React from 'react';
 
-import '../scss/app.scss';
+import axios from 'axios';
+import '../../scss/app.scss';
 import { Category } from './Components/Category';
 import { PizzaBlock } from './Components/PizzaBlock';
+import Skeleton from './Components/PizzaBlock/Skeleton';
 import { Sort } from './Components/Sort';
-
-import axios from 'axios';
 // import pizzas from '../assets/json/pizza.json';
 
 export const Main = () => {
   const [items, setItems] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState(true);
 
   React.useEffect(() => {
     axios
       .get('https://655330845449cfda0f2e4952.mockapi.io/items')
       .then(response => {
         setItems(response.data);
+        setIsLoading(false);
       })
       .catch(() => {
         console.log('Error');
@@ -31,9 +33,9 @@ export const Main = () => {
         </div>
         <h2 className="content__title">Все пиццы</h2>
         <div className="content__items">
-          {items.map(obj => (
-            <PizzaBlock key={obj} {...obj} />
-          ))}
+          {isLoading
+            ? [...new Array(6)].map((_, index) => <Skeleton key={index} />)
+            : items.map(obj => <PizzaBlock key={obj.id} {...obj} />)}
         </div>
       </div>
     </div>
